b2d75e66eea2b95da18708ac0af42997bea54b0c
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-04 15:51:06 -0300

    develop Add initial sctrucure for app.
    - Place a sphere on scene by tap.
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,
+                                  ARSCNDebugOptions.showFeaturePoints]
+        sceneView.session.run(configuration)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tap)
     }
 
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView    = sender.view as? ARSCNView else { return }
+        guard let currentFrame = sceneView.session.currentFrame else { return }
+        
+        let camera    = currentFrame.camera
+        let transform = camera.transform
+        
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.z = -0.1
+        
+        let modifiedMatrix = simd_mul(transform, translationMatrix)
+        
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+    }
 
 }
 
